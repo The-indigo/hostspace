576abd0a4c40c9844c938de07e6c938f44e23f06
The-indigo <adepojuadeyemi11@gmail.com>
2023-10-28 15:44:09 -0400

    fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,7 +87,15 @@ resource "aws_ecs_task_definition" "theyemihostspaceTaskDefinition" {
             "image": "568305562431.dkr.ecr.ca-central-1.amazonaws.com/backend:latest",
             "cpu": 1024,
             "memory": 2048,
-            "portMappings": [],
+                        "portMappings": [
+                {
+                    "name": "backend",
+                    "containerPort": 5000,
+                    "hostPort": 5000,
+                    "protocol": "tcp",
+                    "appProtocol": "http"
+                }
+            ],
             "essential": false,
             "environment": [],
             "environmentFiles": [],
